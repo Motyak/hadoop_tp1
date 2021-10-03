5ede537205fa247fadbf379466587f68720b2ccd
motyak <motyak_@live.fr>
2021-10-03 09:47:53 -0400

    added vscode settings + notes

--- src/WordCount.java
+++ src/WordCount.java
@@ -1,44 +1,44 @@
-import java.io.IOException;
-import java.util.concurrent.ThreadLocalRandom;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-public class WordCount 
+public class WordCount
 {
     public static class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable>
     {
-        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException
+        @Override
+        public void map(LongWritable key, Text value, Context context)
         { 
-            String string = value.toString();
-            if(!string.isEmpty())
-            {
-                Text outText = new Text(string.substring(0,1));
-                int val = ThreadLocalRandom.current().nextInt(1,100);
-                IntWritable outVal = new IntWritable(val);
-                context.write(outText, outVal);
-            }
+            // String string = value.toString();
+            // if(!string.isEmpty())
+            // {
+            //     Text outText = new Text(string.substring(0,1));
+            //     int val = ThreadLocalRandom.current().nextInt(1,100);
+            //     IntWritable outVal = new IntWritable(val);
+            //     context.write(outText, outVal);
+            // }
         }
     }
 
     public static class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable>
     {
-        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException
+        @Override
+        public void reduce(Text key, Iterable<IntWritable> values, Context context)
         {
-            int sum = 0;
-            for(IntWritable value: values)
-            {
-                sum = sum + value.get();
-            }
-            IntWritable outVal = new IntWritable(sum);
-            context.write(key, outVal);
+            // int sum = 0;
+            // for(IntWritable value: values)
+            // {
+            //     sum = sum + value.get();
+            // }
+            // IntWritable outVal = new IntWritable(sum);
+            // context.write(key, outVal);
         }
     }
 
